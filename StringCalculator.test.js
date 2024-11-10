d419f03fa681f75d8bd7c14391570617c1041eda
unknown <chetansonawane2695@gmail.com>
2024-11-10 20:03:56 +0530

    add 2nd test case for should return the number itself when only one number is provided

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -5,4 +5,8 @@ describe("StringCalculator", () => {
   test("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  test("should return the number itself when only one number is provided", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
 });
