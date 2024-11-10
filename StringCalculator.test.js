4b42e5cd0a77aa6dc71fcca80d1a3dfc35f067c3
unknown <chetansonawane2695@gmail.com>
2024-11-10 20:09:16 +0530

    create 6th test case for should handle custom delimiters

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -21,4 +21,9 @@ describe("StringCalculator", () => {
   test("should handle new lines as delimiters", () => {
     expect(calculator.add("1\n2,3")).toBe(6);
   });
+
+  test("should handle custom delimiters", () => {
+    expect(calculator.add("//;\n1;2")).toBe(3);
+    expect(calculator.add("//|\n1|2|3")).toBe(6);
+  });
 });
